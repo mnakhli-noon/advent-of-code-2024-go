a3d2f90710118faa675e7f67df9fd466edb5f8e8
terameso <mnakhli@noon.com>
2024-12-13 20:55:47 +0400

    Day 3 part 2

--- day-03/solution.go
+++ day-03/solution.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -11,7 +12,84 @@ import (
 type acceptedChar struct {
 	value            string
 	nextAcceptedChar string
-	shouldEndAfter   bool
+}
+type condtionalChar struct {
+	value             string
+	doNext            string
+	dontNext          string
+	shouldChangeState bool
+}
+
+var acceptMap = map[string]acceptedChar{
+	"m": {
+		value:            "m",
+		nextAcceptedChar: "u",
+	},
+	"u": {
+		value:            "u",
+		nextAcceptedChar: "l",
+	},
+	"l": {
+		value:            "l",
+		nextAcceptedChar: "(",
+	},
+	"(": {
+		value:            "(",
+		nextAcceptedChar: "digit1",
+	},
+	"digit1": {
+		value:            "digit1",
+		nextAcceptedChar: ",",
+	},
+	"digit2": {
+		value:            "digit2",
+		nextAcceptedChar: ")",
+	},
+}
+
+var conditionalMap = map[string]condtionalChar{
+	"d": {
+		value:             "d",
+		doNext:            "o",
+		dontNext:          "o",
+		shouldChangeState: false,
+	},
+	"o": {
+		value:             "o",
+		doNext:            "(",
+		dontNext:          "n",
+		shouldChangeState: false,
+	},
+	"(": {
+		value:             "(",
+		doNext:            ")",
+		dontNext:          ")",
+		shouldChangeState: false,
+	},
+	")": {
+		value:             ")",
+		doNext:            "d",
+		dontNext:          "d",
+		shouldChangeState: true,
+	},
+	"n": {
+		value:             "n",
+		doNext:            "d",
+		dontNext:          "'",
+		shouldChangeState: false,
+	},
+	"'": {
+		value:             "'",
+		doNext:            "d",
+		dontNext:          "t",
+		shouldChangeState: false,
+	},
+	"t": {
+		value:             "t",
+		doNext:            "d",
+		dontNext:          "(",
+		shouldChangeState: false,
+	},
 }
 
 func checkDigit(character string) bool {
@@ -23,44 +101,11 @@ func checkDigit(character string) bool {
 
 }
 
-func main() {
+func solvePartOne() {
 
 	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
-	}
-
-	acceptMap := map[string]acceptedChar{
-		"m": {
-			value:            "m",
-			nextAcceptedChar: "u",
-			shouldEndAfter:   false,
-		},
-		"u": {
-			value:            "u",
-			nextAcceptedChar: "l",
-			shouldEndAfter:   false,
-		},
-		"l": {
-			value:            "l",
-			nextAcceptedChar: "(",
-			shouldEndAfter:   false,
-		},
-		"(": {
-			value:            "(",
-			nextAcceptedChar: "digit1",
-			shouldEndAfter:   false,
-		},
-		"digit1": {
-			value:            "digit1",
-			nextAcceptedChar: ",",
-			shouldEndAfter:   false,
-		},
-		"digit2": {
-			value:            "digit2",
-			nextAcceptedChar: ")",
-			shouldEndAfter:   false,
-		},
 	}
 
 	scanner := bufio.NewScanner(file)
@@ -73,7 +118,6 @@ func main() {
 
 	for scanner.Scan() {
 		character := scanner.Text()
-
 		if nextAcceptedChar.value == "digit1" {
 			if checkDigit(character) {
 				firstNumber += character
@@ -96,7 +140,6 @@ func main() {
 				if err != nil {
 					log.Fatal("Something is wrong with secondNumber number", secondNumber)
 				}
-
 				result += firstInt * secondInt
 			}
 		}
@@ -110,5 +153,89 @@ func main() {
 	}
 
 	fmt.Println("Result is: ", result)
+}
+
+func solvePartTwo() {
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanBytes)
+	nextAcceptedChar := acceptMap["m"]
+	nextConditionalChar := conditionalMap["d"]
+	firstNumber := ""
+	secondNumber := ""
+	result := 0
+	enabled := true
+
+	for scanner.Scan() {
+		character := scanner.Text()
+		if enabled {
+			if nextAcceptedChar.value == "digit1" {
+				if checkDigit(character) {
+					firstNumber += character
+					continue
+				} else if character == nextAcceptedChar.nextAcceptedChar {
+					nextAcceptedChar = acceptMap["digit2"]
+					continue
+				}
+			} else if nextAcceptedChar.value == "digit2" {
+				if checkDigit(character) {
+					secondNumber += character
+					continue
+				} else if character == nextAcceptedChar.nextAcceptedChar {
+					firstInt, err := strconv.Atoi(firstNumber)
+					if err != nil {
+						log.Fatal("Something is wrong with first number", firstNumber)
+					}
+
+					secondInt, err := strconv.Atoi(secondNumber)
+					if err != nil {
+						log.Fatal("Something is wrong with secondNumber number", secondNumber)
+					}
+					result += firstInt * secondInt
+				}
+			}
+			if character != nextAcceptedChar.value {
+				firstNumber = ""
+				secondNumber = ""
+				nextAcceptedChar = acceptMap["m"]
+			} else {
+				nextAcceptedChar = acceptMap[nextAcceptedChar.nextAcceptedChar]
+			}
+		}
+
+		if nextAcceptedChar.value == "m" {
+			if character == nextConditionalChar.value {
+
+				if enabled {
+					nextConditionalChar = conditionalMap[nextConditionalChar.dontNext]
+				} else {
+					nextConditionalChar = conditionalMap[nextConditionalChar.doNext]
+				}
+				if nextConditionalChar.shouldChangeState {
+					enabled = !enabled
+				}
+			} else {
+				nextConditionalChar = conditionalMap["d"]
+			}
+		}
+	}
+
+	fmt.Println("Result is: ", result)
+}
+
+func main() {
+	isPartTwo := flag.Bool("partTwo", false, "Solve part two")
+	flag.Parse()
+	if *isPartTwo {
+		solvePartTwo()
+	} else {
+		solvePartOne()
+	}
 
 }
