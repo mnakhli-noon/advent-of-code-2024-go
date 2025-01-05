f008f22a5f03af1503206ff234fed0723697d803
terameso <mnakhli@noon.com>
2025-01-05 15:47:42 +0400

    Part 1, better typing

--- day-06/solution.go
+++ day-06/solution.go
@@ -8,7 +8,36 @@ import (
 	"slices"
 )
 
-func readInput() ([][]rune, int, int) {
+type Grid [][]rune
+type WalkDirection int
+
+const (
+	UP WalkDirection = iota
+	RIGHT
+	DOWN
+	LEFT
+)
+
+type Position struct {
+	x int
+	y int
+}
+
+var walkPosition = map[WalkDirection]Position{
+	UP:    {x: -1, y: 0},
+	RIGHT: {x: 0, y: 1},
+	DOWN:  {x: 1, y: 0},
+	LEFT:  {x: 0, y: -1},
+}
+
+var nextDircetion = map[WalkDirection]WalkDirection{
+	UP:    RIGHT,
+	RIGHT: DOWN,
+	DOWN:  LEFT,
+	LEFT:  UP,
+}
+
+func readInput() (Grid, int, int) {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -29,91 +58,43 @@ func readInput() ([][]rune, int, int) {
 
 	return lab, x, y
 }
-func checkIfWeCanWalk(maze [][]rune, x int, y int) (bool, bool) {
-	if x < 0 || x >= len(maze) || y < 0 || y >= len(maze[x]) {
-		return false, true
+
+func (g Grid) checkIfWeCanWalk(x int, y int, dir WalkDirection) (bool, bool, int, int) {
+	delta := walkPosition[dir]
+	newX := x + delta.x
+	newY := y + delta.y
+
+	if newX < 0 || newX >= len(g) || newY < 0 || newY >= len(g[newX]) {
+		return false, true, -1, -1
 	}
-	if maze[x][y] == '#' {
-		return false, false
+
+	if g[newX][newY] == '#' {
+		return false, false, x, y
 	}
-	return true, false
+
+	return true, false, newX, newY
 }
 
-func walkTheMaze(maze [][]rune, x int, y int) {
-	if x < 0 || x >= len(maze) || y < 0 || y >= len(maze[x]) {
+func (g Grid) walkTheMaze(x int, y int, dir WalkDirection) {
+	if x < 0 || x >= len(g) || y < 0 || y >= len(g[x]) {
 		return
 	}
-	switch maze[x][y] {
-	case '^':
-		if canWalk, finish := checkIfWeCanWalk(maze, x-1, y); canWalk {
-			maze[x][y] = 'X'
-			maze[x-1][y] = '^'
-			walkTheMaze(maze, x-1, y)
-			return
-		} else {
-			if finish {
-				return
-			} else {
-				maze[x][y] = '>'
-				walkTheMaze(maze, x, y)
-				return
-			}
-		}
-	case '>':
-		if canWalk, finish := checkIfWeCanWalk(maze, x, y+1); canWalk {
-			maze[x][y] = 'X'
-			maze[x][y+1] = '>'
-			walkTheMaze(maze, x, y+1)
-			return
-		} else {
-			if finish {
-				maze[x][y] = 'X'
-				return
-			} else {
-				maze[x][y] = 'v'
-				walkTheMaze(maze, x, y)
-				return
-			}
-		}
-	case 'v':
-		if canWalk, finish := checkIfWeCanWalk(maze, x+1, y); canWalk {
-			maze[x][y] = 'X'
-			maze[x+1][y] = 'v'
-			walkTheMaze(maze, x+1, y)
-			return
-		} else {
-			if finish {
-				maze[x][y] = 'X'
-				return
-			} else {
-				maze[x][y] = '<'
-				walkTheMaze(maze, x, y)
-				return
-			}
-		}
-	case '<':
-		if canWalk, finish := checkIfWeCanWalk(maze, x, y-1); canWalk {
-			maze[x][y] = 'X'
-			maze[x][y-1] = '<'
-			walkTheMaze(maze, x, y-1)
-			return
-		} else {
-			if finish {
-				maze[x][y] = 'X'
-				return
-			} else {
-				maze[x][y] = '^'
-				walkTheMaze(maze, x, y)
-				return
-			}
-		}
-	default:
+	canWalk, finish, newX, newY := g.checkIfWeCanWalk(x, y, dir)
+
+	if canWalk {
+		g[x][y] = 'X'
+		g.walkTheMaze(newX, newY, dir)
+	} else if finish {
+		g[x][y] = 'X'
 		return
+	} else {
+		g.walkTheMaze(x, y, nextDircetion[dir])
 	}
 }
+
 func solvePartOne() {
 	lab, x, y := readInput()
-	walkTheMaze(lab, x, y)
+	lab.walkTheMaze(x, y, UP)
 	count := 0
 	for _, line := range lab {
 		for _, character := range line {
@@ -123,6 +104,10 @@ func solvePartOne() {
 		}
 	}
 	fmt.Println(count)
+}
+
+func solvePartTwo() {
+	fmt.Println("hi")
 }
 
 func main() {
